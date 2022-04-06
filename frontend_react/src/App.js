5f5eea234151d3477a924d5606bed74c3647a702
divine ajie <ezeajie14@gmail.com>
2022-04-06 12:00:42 -0600

    Added files

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const App = () => {
+
+const App = () =>
+
+ {
 
   return (
-    <h1>App</h1>
 
+   <h1>App lets go </h1>
+   
+    
   );
 }
 
--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
+
 import App from './App';
 import './index.css';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+var elem =ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(elem);
