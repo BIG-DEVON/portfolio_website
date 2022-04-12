8d229c8deb3cd88f2536096a8fa7991566d473ac
divine ajie <ezeajie14@gmail.com>
2022-04-12 12:43:54 -0600

    added assets folder

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import './Navbar.scss'
+import { images } from '../../constants';
 
 const Navbar = () => {
     return (
         <nav> 
         
-        <div>
-            <img src={Images.logo} alt="logo/>
-        </div>
+            <div>
+
+                <img src={images.logo} alt= "logo" />
+        
+            </div>
+
+            <ul>
+            {['homes ', 'about', 'contact', 'work', 'skills', 'contact'].map((item) => (
+
+                <li key={`link-${item}`}>
+
+                    <div/>
+                    <a  href={`#${item}`}>{item} </a>
+
+                </li>
+
+            ))}
+
+            </ul>
 
      </nav>
     )
