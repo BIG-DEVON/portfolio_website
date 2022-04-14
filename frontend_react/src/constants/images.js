40de17a9e6fb4957cabd1f515ea31d37c50cd5e0
divine ajie <ezeajie14@gmail.com>
2022-04-14 00:06:38 -0600

    added some styling

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -27,6 +27,8 @@ import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 
+
+
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
 import asus from '../assets/asus.png';
