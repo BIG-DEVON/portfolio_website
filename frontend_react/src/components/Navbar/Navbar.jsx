2a3ae00a8dd08f4f5b048cfd7c3adbc39612b213
divine ajie <ezeajie14@gmail.com>
2022-04-23 00:55:45 -0600

    ..

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
+import { icons } from 'react-icons/lib';
 
 import { images } from '../../constants';
 import './Navbar.scss';
