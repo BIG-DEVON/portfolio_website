d6c126fec28fa75523f1614dbd4cb8061f4b8dfc
BIG-DEVON <ezeajie14@gmail.com>
2022-05-14 22:38:09 -0600

    ..

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -83,7 +83,8 @@ const Header = () => {
           className ="app__header-circles"
         
         >
-        {[images.flutter,images.redux, images.sass].map((circle, index) =>(
+        {[images.flutter,images.redux, images.sass].map((circle, index) =>
+        (
 
         <div className='circle-cmp app__flex ' key={`circle-${index}`}>
           <img  src={ circle} alt="circle" />
