b85b8b521fdaca66484e99dc8f012c471463cd45
BIG-DEVON <ezeajie14@gmail.com>
2022-05-19 10:05:32 +0100

    connected the frontend to the backend

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from 'react';
 import {motion } from 'framer-motion';
 import { useState, useEffect } from 'react';
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
 import './About.scss';
 
-const abouts = [
+ /* const abouts = [
   { title: 'Web Developmment & Design' , description: 'I am a passionate frontend Developer ready to bring your imaginations to life.' , imgUrl: images.about01},
   { title: 'Web  ' , description: 'I am a good web developer ' , imgUrl: images.about02},
   { title: 'Frontend' , description: '  ' , imgUrl:  images.about03},
   { title: 'Cloud Computing' , description: 'Up and ready to to provide soft solutions to clients tasks. ' , imgUrl:images.about04},
-];
+]; */
 
 
-const About = () => {
+  const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+    .then((data) =>    setAbouts(data))
+
+  
+  }, []);
+
   return (
     <>
-      <h2 className='head-text '>
-         I Know that 
+      <h2 className='head-text'>
+        I Know that 
       <span> Great Dev</span>
       <br/>
         Means
@@ -29,14 +42,18 @@ const About = () => {
 
         <motion.div   
         whileInView={{ opacity : 1}}
-        whileHover={{ scale:1.1 }}
-        transition ={{ duration: 0.5, type: 'tween'}}
-        className= "app__profile-item"
-        key={about.title + index }
+
+        whileHover= {{ scale:1.1 }}
+
+        transition = { { duration: 0.5, type: 'tween'}}
+
+        className = "app__profile-item"
+        key = {about.title + index }
+
         >
 
-        <img src={about.imgUrl} alt={about.title}/>
-        <h2 className='bold-text ' style={{marginTop: 20 }} >{about.title } </h2>
+        <img src={urlFor(about.imgUrl)} alt={about.title}/>
+        <h2 className='bold-text ' style={{marginTop: 20 }} > {about.title } </h2>
         <p className='p-text ' style={{marginTop: 10 }} >{about.description} </p>
 
 
