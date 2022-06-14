30fd9628e29c97bf38b66db37174647ce07fcc7f
BIG-DEVON <ezeajie14@gmail.com>
2022-06-14 11:10:35 +0100

    ..

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -3,35 +3,34 @@ import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebook } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { BsGithub } from 'react-icons/bs';
+import { useState, useEffect } from 'react';
 
 
 const SocialMedia = () => {
+
+
+    const [url, setUrl] = useState('')
+    
   return (
     <div className='app__social'>
 
-        <div>
-
-            <BsTwitter />
-
-        </div>
-
-        <div>
-
-            <FaFacebook />
-
+        <div> 
+        <a target="_blank" href={"https://twitter.com/Devon5335"} >
+            <BsTwitter  />
+            </a> 
         </div>
 
 
-
         <div>
-
+        <a target="_blank" href={""} >
             <BsInstagram />
-
+            </a>
         </div>
 
         <div>
-
-        <FaGithub />
+        <a target="_blank" href={" https://github.com/BIG-DEVON"} >
+        <FaGithub  />
+        </a>
 
         </div>
 
